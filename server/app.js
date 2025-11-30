84af788bff5280bba8c41b0c8f826b7fd91d20c6
Jin Yi <jhy8406@g.rit.edu>
2025-11-30 00:56:53 -0500

    more temporary comment out

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const favicon = require('serve-favicon');
+//const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
