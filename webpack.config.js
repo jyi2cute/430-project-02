67d2d589302a280d929114c2e92d2526ff21f64e
Jin Yi <jhy8406@g.rit.edu>
2025-11-29 20:08:46 -0500

    added in the run version of webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path'); //For resolving file paths
+module.exports = {
+    entry: {
+        app: './client/maker.jsx',
+        login: './client/login.jsx',
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+     //The 'main' file of your bundle
+    mode: 'production', //Development mode lets us debug client code
+    watchOptions: {
+        aggregateTimeout: 200, //Prevents multiple rebuilds on save
+    },
+    output: {
+        //Where to put the bundle
+        path: path.resolve(__dirname, 'hosted'),
+        filename: '[name]bundle.js', //The name of the bundle file
+    }
+}
