fb6456f26863c1f3e036a48f9dfd374a1f15fcf5
Jin Yi <jhy8406@g.rit.edu>
2025-12-09 03:09:20 -0500

    added fixes onto the styling

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -113,9 +113,9 @@ const premiumUpgrade = async (req, res) => {
     const account = await Account.findByUsername(req.session.account.username);
 
     if (!account) {
-      return res.status(404).json({ error: 'Account not found'});
+      return res.status(404).json({ error: 'Account not found' });
     }
-    
+
     if (account.isPremium) {
       return res.status(200).json({ message: 'You are already MoodBoard PRO!' });
     }
--- server/models/Account.js
+++ server/models/Account.js
@@ -40,7 +40,7 @@ const AccountSchema = new mongoose.Schema({
   isPremium: {
     type: Boolean,
     default: false,
-  }
+  },
 });
 
 // Converts a doc to something we can store in redis later on.
